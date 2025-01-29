6e899b54a3b5239b4bd64bf2b3d7bb012a2c50a1
mhaouas <mat.haouas@gmail.com>
2025-01-29 14:09:40 +0100

    Creating a scrypt to create a virtual environement to start the project on the right foot.

---
+++ srcs/install_venv.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if requirements.txt exists
+if [ ! -f requirements.txt ]; then
+  echo "requirements.txt not found!"
+  exit 1
+fi
+
+# Create a virtual environment named 'venv'
+python3 -m venv .venv
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Install the dependencies from requirements.txt
+pip install -r requirements.txt
+
+# Deactivate the virtual environment
+deactivate
+
+echo "Virtual environment created and dependencies installed successfully."
