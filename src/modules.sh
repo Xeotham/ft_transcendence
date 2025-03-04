35d41ca3712d4701adad7e037f057eae10b5c4f3
mhaouas <mat.haouas@gmail.com>
2025-03-04 18:41:48 +0100

    Creating modules.sh to install package and updating gitignore to ignore node_modules

---
+++ src/modules.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+npm install;																																			# install node package manager
+npm update npm -g;																																# update it
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash;
+nvm install 22;																																		# update it again
+node -v;																																					# Should print "v22.13.1".
+npm install --save-dev ts-node-dev typescript @types/node
+npm install -D typescript;
+npm i fastify;																																		# install fastify
+npm i @fastify/websocket;																													# install websockets
+npm install @fastify/static;																											# install static Handler
+npm install better-sqlite3;																												# install better-sqlite3 for the DataBase
+npm i @types/ws -D;																																# definitions for websockets for typescript
+npm install path;																																	# install node path extension
+npm install dotenv --save;																												# install dotenv to handle environement variables
+npm init -y;
+npm audit fix;																																		# automaticly fixes problems if any are encountered
