aa1ce4acc353dc687c217be3e0d97113ef2fd08d
bchene <bchene@student.42angouleme.fr>
2025-04-29 15:21:49 +0200

    commit de secu avant changer branche deso...

--- src/front/transcendence-front/vite.config.js
+++ src/front/transcendence-front/vite.config.js
@@ -1,7 +1,11 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
     server: {
         port: DUMMY_FRONT_PORT,
         hot: true,
