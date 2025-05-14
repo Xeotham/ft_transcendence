9701550288f00a317c5d1b8bf6d21b3a0ef6f6f1
Basile Vasseur <bvasseur@student.42angouleme.fr>
2025-05-14 14:58:41 +0200

    Tetris - added various sounds. Revamp of the Tetrimino class and json. It is now more flexible and easier to use for future uses like adding pieces

--- src/back/transcendenceBack/tetris_app/server/Game/Pieces/L.ts
+++ src/back/transcendenceBack/tetris_app/server/Game/Pieces/L.ts
@@ -2,25 +2,18 @@ import { ATetrimino } from "../ATetrimino";
 import { IPos } from "../IPos";
 import * as tc from "../tetrisConstants";
 import LJson from "./LJson.json";
-import {Matrix} from "../Matrix";
+import { Matrix } from "../Matrix";
 
 export class L extends ATetrimino {
 
-	protected static readonly SpinCheck: number[][] = [
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 1, 0, 0],
-		[0, 0, 1, 1, 1, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-	]; // 2 major, 3 minor
+	protected static readonly SpinCheck: number[][] = [[]]; // 2 major, 3 minor
 
 
 	// Load the JSON file and convert it to the pieceStruct
 	protected static struct: tc.pieceStruct = (() => {
 		return {
 			size: LJson.size,
+			nbBlocks: LJson.nbBlocks,
 			north: this.convertBlock(LJson.north),
 			east: this.convertBlock(LJson.east),
 			south: this.convertBlock(LJson.south),
