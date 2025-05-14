6e787595217742dedbc94b16429d7d3b5acb8edb
mhaouas <mhaouas@student.42angouleme.fr>
2025-05-14 10:20:45 +0200

    Setup for Tetris's SFX and added BGM.
    Need to rebase with tetris_back to test sfx.

--- src/front/transcendenceFront/src/ts/tetris/htmlPage.ts
+++ src/front/transcendenceFront/src/ts/tetris/htmlPage.ts
@@ -96,7 +96,6 @@ const keyBindsHtml = (keys: keys) => {
 // 		<p id="PPS">Pieces: 0, 0.00/S</p>
 // <!--		<p id="level">Level: 1</p>-->
 // <!--		<p id="lines">Lines: 0/10</p>-->
-// <!--		<p id="awaitingGarbage">Incoming Garbage: 0</p>-->
 // 		<canvas id="gameCanvas" width="${canvasWidth}" height="${canvasHeight}"></canvas>
 // 	`
 // }
@@ -108,7 +107,14 @@ const boardHtml = () => {
 
 	content.innerHTML = `
 <!--		<p id="score">Score: 0</p>-->
-		<p id="awaitingGarbage">Incoming Garbage: 0</p>
+		<div id="sfx">
+		<audio id="sfxplayer"/>
+<!--		<audio id="allclear" src="./src/sfx/tetris/Tetrio_retro_pack/allclear.wav" />-->
+<!--		<audio id="applause" src="./src/sfx/tetris/Tetrio_retro_pack/applause.wav" />-->
+<!--		<audio id="boardappear" src="./src/sfx/tetris/Tetrio_retro_pack/boardappear.wav" />-->
+<!--		<audio id="btb_1" src="./src/sfx/tetris/Tetrio_retro_pack/btb_1.wav" />-->
+<!--		<audio id="btb_2" src="./src/sfx/tetris/Tetrio_retro_pack/btb_2.wav" />-->
+		</div>
 		<div id="board">
 		<canvas id="tetrisCanvas" width="${window.innerWidth}" height="${window.innerHeight}"></canvas>
 		</div>`
