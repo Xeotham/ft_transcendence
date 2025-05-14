0b814cb9ace098276ec5c2c1a18122b5b44be496
mhaouas <mhaouas@student.42angouleme.fr>
2025-05-14 10:20:45 +0200

    Setup for Tetris's SFX and added BGM.
    Need to rebase with tetris_back to test sfx.

--- src/front/transcendenceFront/src/ts/tetris/tetrisHTML.ts
+++ src/front/transcendenceFront/src/ts/tetris/tetrisHTML.ts
@@ -108,6 +108,14 @@ const boardHtml = () => {
 
 	EL.contentTetris.innerHTML = `
 <!--		<p id="score">Score: 0</p>-->
+		<div id="sfx">
+		<audio id="sfxplayer"/>
+<!--		<audio id="allclear" src="./src/sfx/tetris/Tetrio_retro_pack/allclear.wav" />-->
+<!--		<audio id="applause" src="./src/sfx/tetris/Tetrio_retro_pack/applause.wav" />-->
+<!--		<audio id="boardappear" src="./src/sfx/tetris/Tetrio_retro_pack/boardappear.wav" />-->
+<!--		<audio id="btb_1" src="./src/sfx/tetris/Tetrio_retro_pack/btb_1.wav" />-->
+<!--		<audio id="btb_2" src="./src/sfx/tetris/Tetrio_retro_pack/btb_2.wav" />-->
+		</div>
 		<div id="board">
 		<canvas id="tetrisCanvas" width="${window.innerWidth}" height="${window.innerHeight}"></canvas>
 		</div>`
